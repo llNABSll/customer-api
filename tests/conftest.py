c2f60d46680f02d229dd10fb075d5df8d3b65dbc
Benjamin PRUJA <benjamin.pruja@tmigi.com>
2025-09-09 15:03:35 +0200

    fix : coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 # tests/conftest.py
+import os
+import sys
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app.main import app
 from app.core.database import Base, get_db
